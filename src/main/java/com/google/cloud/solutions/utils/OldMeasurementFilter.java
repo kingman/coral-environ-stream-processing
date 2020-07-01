fb453fe93ba71ee973820998e0610638b4d077d5
Charlie Wang <charlie.zwang@gmail.com>
2020-07-02 00:32:23 +0200

    make filter serializable

--- src/main/java/com/google/cloud/solutions/utils/OldMeasurementFilter.java
+++ src/main/java/com/google/cloud/solutions/utils/OldMeasurementFilter.java
@@ -2,16 +2,19 @@ package com.google.cloud.solutions.utils;
 
 import com.google.cloud.solutions.common.Measurement;
 
+import org.apache.beam.sdk.transforms.SerializableFunction;
 import org.joda.time.Duration;
 import org.joda.time.Instant;
 
-public class OldMeasurementFilter {
+public class OldMeasurementFilter implements SerializableFunction<Measurement, Boolean>{
+    private static final long serialVersionUID = -6889744369750410224L;
     private final int allowedSkewMinutes;
 
     public OldMeasurementFilter(int allowedSkewMinutes) {
         this.allowedSkewMinutes = allowedSkewMinutes;
     }
 
+    @Override
     public Boolean apply(Measurement measurement) {
         Instant breakInstant = Instant.now().minus(Duration.standardMinutes(allowedSkewMinutes));
         return measurement.getTimestamp().isAfter(breakInstant);
